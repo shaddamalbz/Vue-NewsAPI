b92eb321815bf2bf0c7820aa288814c01ada28f7
shaddamalbz <shaddam.albz@gmail.com>
2023-07-04 11:59:04 +0700

    [deps] add antd

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 import 'ant-design-vue/dist/antd.css'
 
 import { createApp } from 'vue'
+import antd from 'ant-design-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -9,5 +10,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-
+app.use(antd)
 app.mount('#app')
